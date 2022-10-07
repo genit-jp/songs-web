4851c69453a3d631d0d300ab95d9324cf591f8a5
ryohorie <develop@ryohorie.com>
2022-10-07 20:15:26 +0900

    リンクを変更

--- components/InstallButton.vue
+++ components/InstallButton.vue
@@ -2,20 +2,20 @@
   v-container(style="padding:0px;")
     v-row(align="center" justify="center" style="margin:0; padding:0;")
       v-col(cols="5")
-        a(href="" target="_blank")
+        a(href="" target="https://apps.apple.com/jp/app/id1565104504")
           v-img(
           src="images/badges/ios.svg"
           max-height="80"
           contain
         )
       v-col(cols="6")
-        a(href="" target="_blank")
+        a(href="" target="https://play.google.com/store/apps/details?id=jp.genit.songs")
           v-img(
           src="images/badges/android.png"
           max-height="80"
           contain
         )
-      v-col.text-center.text-sm-body-1.text-caption(cols="12") ※ダウンロード無料 / 有料会員:月額1200円
+      v-col.text-center.text-sm-body-1.text-caption(cols="12") ※ダウンロード無料 / 有料会員:月額1600円
 </template>
 <script>
 export default {}

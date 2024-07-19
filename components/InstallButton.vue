0aac04be0db8e9aff9c2e957665ea9fa2430d1ca
ryohorie <develop@ryohorie.com>
2024-07-19 11:38:00 +0900

    金額を修正

--- components/InstallButton.vue
+++ components/InstallButton.vue
@@ -8,14 +8,14 @@
           max-height="80"
           contain
         )
-      v-col(cols="6")
-        a(href="https://play.google.com/store/apps/details?id=jp.genit.songs" target="_blank")
-          v-img(
-          src="images/badges/android.png"
-          max-height="80"
-          contain
-        )
-      v-col.text-center.text-sm-body-1.text-caption(cols="12") ※ダウンロード無料 / 有料会員:月額1600円
+      //- v-col(cols="6")
+      //-   a(href="https://play.google.com/store/apps/details?id=jp.genit.songs" target="_blank")
+      //-     v-img(
+      //-     src="images/badges/android.png"
+      //-     max-height="80"
+      //-     contain
+      //-   )
+      v-col.text-center.text-sm-body-1.text-caption(cols="12") ※ダウンロード無料 / 有料会員:月額600円
 </template>
 <script>
 export default {}
